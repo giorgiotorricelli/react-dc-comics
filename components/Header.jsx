059b64c26f8398a3f996c91d63265938f4757990
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-27 16:17:42 +0200

    aggiunto stile header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,15 @@
 import Nav_header from "./Nav_header";
 
 function Header(){
-    return <header>
-        <div className="logo-wrapper"><img src="/img/dc-logo.png" alt="dc logo" /></div>
-        <Nav_header/>
-            </header>;
+    return <header className="height-header">
+        <div className="container dis-flex align-center space-between" style={{height : '100%'}}>
+            <div className="logo-wrapper" style={{height : '85%'}}>
+                <img src="/img/dc-logo.png" alt="dc logo" style={{height : '100%'}}/>
+            </div>
+            <Nav_header/>
+        </div>
+        
+    </header>;
 }
 
 export default Header
