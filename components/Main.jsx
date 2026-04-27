f3bf41e792663557091d98cc69493580ebf4f182
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-27 16:46:30 +0200

    aggiunto main

---
+++ components/Main.jsx
@@ -0,0 +1,11 @@
+
+
+function Main(){
+    return <main>
+        <div className="container content-wrapper">
+            <h3>--&gt;Content goes here&lt;--</h3>
+        </div>
+    </main>;
+}
+
+export default Main
