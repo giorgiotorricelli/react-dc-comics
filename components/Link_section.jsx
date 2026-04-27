cb930d81051dafc824eb1ee9bcbedec7af629f71
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-27 17:27:32 +0200

    fascia azzurra con icone fatta con componente aggiuntivo

---
+++ components/Link_section.jsx
@@ -0,0 +1,31 @@
+
+
+function Link_section(){
+    return <section>
+        <div className="section-wrapper dis-flex align-center container">
+            <div className="go-to dis-flex align-center">
+                <div className="img-wrapper"><img src="/img/buy-comics-digital-comics.png" alt="" /></div>
+                <a href="">DIGITAL COMICS</a>
+            </div>
+            <div className="go-to dis-flex align-center">
+                <div className="img-wrapper"><img src="/img/buy-comics-merchandise.png" alt="" /></div>
+                <a href="">DC MERCHANDISE</a>
+            </div>
+            <div className="go-to dis-flex align-center">
+                <div className="img-wrapper"><img src="/img/buy-comics-subscriptions.png" alt="" /></div>
+                <a href="">SUBSCRIPTION</a>
+            </div>
+            <div className="go-to dis-flex align-center">
+                <div className="img-wrapper"><img src="/img/buy-comics-shop-locator.png" alt="" /></div>
+                <a href="">COMIC SHOP LOCATOR</a>
+            </div>
+            <div className="go-to dis-flex align-center">
+                <div className="img-wrapper" id="img-svg"><img src="/img/buy-dc-power-visa.svg" alt="" /></div>
+                <a href="">DC POWER VISA</a>
+            </div>
+            
+        </div>
+    </section>
+}
+
+export default Link_section
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Main from "../components/Main";
+import Link_section from "../components/Link_section";
 
 function App() {
 
   return <>
     <Header/>
     <Main/>
+    <Link_section/>
   </>;
 }
 
