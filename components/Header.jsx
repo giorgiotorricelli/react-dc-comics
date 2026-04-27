3e200b78b1a54e675103ccc279c69e05fb35f2db
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-27 15:46:14 +0200

    creato Header senza alcuno stile

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+import Nav_header from "./Nav_header";
+
+function Header(){
+    return <header>
+        <div className="logo-wrapper"><img src="/img/dc-logo.png" alt="dc logo" /></div>
+        <Nav_header/>
+            </header>;
+}
+
+export default Header
